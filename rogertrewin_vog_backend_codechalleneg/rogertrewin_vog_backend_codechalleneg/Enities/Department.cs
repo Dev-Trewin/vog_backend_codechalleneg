6a70c57c46c77c6f7ee11a92065bd7e24d61c264
Dev-Trewin <trewinroger.r@gmail.com>
2022-01-27 21:47:06 -0700

    Add project entities

---
+++ rogertrewin_vog_backend_codechalleneg/rogertrewin_vog_backend_codechalleneg/Enities/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ rogertrewin_vog_backend_codechalleneg/rogertrewin_vog_backend_codechalleneg/Enities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Entities
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public int DeparmentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string MailingAddress { get; set; }
+
+    }
+}
