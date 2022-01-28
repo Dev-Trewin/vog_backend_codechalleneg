787430bf098952199de06b0e196d5cc8b8f04ba6
Dev-Trewin <trewinroger.r@gmail.com>
2022-01-27 21:54:42 -0700

    Add employees endpoint

---
+++ rogertrewin_vog_backend_codechalleneg/rogertrewin_vog_backend_codechalleneg/Controllers/EmployeesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VogCodeChallenge.API.Services;
+
+namespace VogCodeChallenge.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeService _service;
+
+        public EmployeesController(IEmployeeService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employees = _service.GetAll();
+            return Ok(employees);
+        }
+
+        [Route("department/{departmentId:int}")]
+        [HttpGet]
+        public IActionResult GetEmployeesByDepartmentId(int departmentId)
+        {
+            var employees = _service.GetEmployeesByDepartmentId(departmentId);
+            return Ok(employees);
+        }
+
+
+    }
+
+}
